ee63eba9c6812915b16c9d5e39d3b36e9e602729
Leonardo Garruba <leonardogarrubaaraujo@gamil.com>
2020-07-25 22:04:05 -0300

    Routes and Fonts configuredRoutes and Fonts configured

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
+import { AppLoading } from 'expo'
+import { useFonts, Raleway_800ExtraBold, Raleway_700Bold, Raleway_600SemiBold } from '@expo-google-fonts/raleway'
+import { Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold, Poppins_300Light, Poppins_400Regular } from '@expo-google-fonts/poppins'
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Raleway_800ExtraBold,
+    Raleway_700Bold,
+    Raleway_600SemiBold,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
